d3e83f8873d06fa3cec11927d1cee8401959dc80
mstoyano <mom4il13@hotmail.com>
2015-03-26 19:45:00 +0100

    Nuevo views

--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from models import Pages
 
-# Create your views here.
+
+def handler(request, resource):
+	fila = Pages.objects.filter(name=resource)
+	if len(fila) == 0:
+		fila = Pages(name=resource, page="Pagina de " + resource)
+		fila.save()
+		return HttpResponse(fila.page)
+	else:
+		return HttpResponse(fila[0].page)
